6db023c531241d90e117e3a34dfd71b0ae4e698a
khawarwaraich1995 <57666092+khawarwaraich1995@users.noreply.github.com>
2022-02-02 23:52:17 +0500

    cors

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,11 @@
 <?php
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS, post, get');
+header("Access-Control-Max-Age", "3600");
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+header("Access-Control-Allow-Credentials", "true");
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\Client\AuthController as ClientAuthController;
